f3d3a4f312bc9ecee562849dccda5b0e681f0fcf
Susie Douang <sdouang@Susies-MacBook-Pro.local>
2024-01-27 09:59:04 -0600

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,25 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/brilliancy/",
+  base: "/brilliancycloud/",
   plugins: [react()],
+  // Development server settings.
+  server: {
+    port: 3000, // The port on which the development server will run.
+  },
+
+  // Build configuration.
+  build: {
+    outDir: "dist", // Output directory for production build.
+    assetsDir: "assets", // Directory for static assets in the build output.
+    minify: "terser", // Minification options (terser or esbuild).
+  },
+
+  // Plugins configuration.
+  plugins: [
+    // Add any Vite plugins here.
+  ],
+
+  // Additional configurations.
+  // ...
 });
